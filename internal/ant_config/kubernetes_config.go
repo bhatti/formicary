6dafbd437708170aa5ed51e059a0b37031bba7cb
Shahzad Bhatti <bhatti@plexobject.com>
2026-09-19 11:32:00 -0700

    updating k8 memory limit

--- internal/ant_config/kubernetes_config.go
+++ internal/ant_config/kubernetes_config.go
@@ -728,7 +728,7 @@ func (kc *KubernetesConfig) Validate() error {
 		// api.ResourceList is not decodable by viper/mapstructure from string values,
 		// so max_limits in the config file is silently ignored. Use a sensible default
 		// that matches the typical k8s node capacity for AI workloads.
-		kc.MaxLimits, _ = utils.CreateResourceList("8.0", "8G", "8G")
+		kc.MaxLimits, _ = utils.CreateResourceList("32.0", "32G", "32G")
 	}
 	if kc.MaxServicesPerPod <= 0 {
 		kc.MaxServicesPerPod = 100
